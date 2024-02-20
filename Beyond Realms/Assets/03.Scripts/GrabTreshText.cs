750c40ef495faf8494f69b6866948f952435d5dd
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-02-20 19:39:12 +0900

    Feat: 나레이터 추가

--- Beyond Realms/Assets/03.Scripts/GrabTreshText.cs
+++ Beyond Realms/Assets/03.Scripts/GrabTreshText.cs
@@ -1,41 +1,72 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 
 public class GrabTreshText : MonoBehaviour
 {
     public XRGrabInteractable[] grabInteractables;
-    public GameObject uiTextTreshObject; 
+    public GameObject uiPanelObject;
+    public Text uiText;
 
+    private CanvasGroup canvasGroup;
 
     void Start()
     {
         foreach (var grabInteractable in grabInteractables)
         {
-            // Grab Interactable에 이벤트 리스너 추가
             grabInteractable.onSelectEntered.AddListener(OnGrab);
             grabInteractable.onSelectExited.AddListener(OnRelease);
+        }
+
+        if (uiPanelObject != null)
+        {
+            canvasGroup = uiPanelObject.GetComponent<CanvasGroup>();
+            if (canvasGroup == null)
+            {
+                canvasGroup = uiPanelObject.AddComponent<CanvasGroup>();
+            }
+        }
+
+        if (uiText != null)
+        {
+            uiText.gameObject.SetActive(false);
         }
     }
 
     void OnGrab(XRBaseInteractor interactor)
+    {
+        if (canvasGroup != null)
         {
-            if (uiTextTreshObject != null)
+            uiPanelObject.SetActive(true);
+            canvasGroup.alpha = 1f;
+
+            if (uiText != null)
             {
-                uiTextTreshObject.SetActive(true); // UI Text 활성화
+                uiText.gameObject.SetActive(true);
             }
-
         }
+    }
 
-        void OnRelease(XRBaseInteractor interactor)
+    void OnRelease(XRBaseInteractor interactor)
+    {
+        if (canvasGroup != null)
         {
-            if (uiTextTreshObject != null)
-            {
-                uiTextTreshObject.SetActive(false); // UI Text 비활성화
-            }
+            canvasGroup.alpha = 0f;
 
+            StartCoroutine(DisablePanelAndTextAfterDelay(0.3f));
         }
+    }
 
+    IEnumerator DisablePanelAndTextAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
 
+        uiPanelObject.SetActive(false);
 
-
+        if (uiText != null)
+        {
+            uiText.gameObject.SetActive(false);
+        }
+    }
 }
