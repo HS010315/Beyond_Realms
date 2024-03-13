f8d8e7d5a600ddd45e4aba556696b585d733a719
HS010315 <hspower01@gmail.com>
2024-03-13 18:37:59 +0900

    Feat : "특정 각도 도달 시 고정 후 이벤트 발생"
    + 에니그마박스 프리팹수정

--- Beyond Realms/Assets/03.Scripts/KeyInteraction.cs
+++ Beyond Realms/Assets/03.Scripts/KeyInteraction.cs
@@ -15,16 +15,23 @@ public class KeyInteraction : MonoBehaviour
             float currentAngle = rotateKey.transform.rotation.eulerAngles.z;
             Destroy(gameObject);
             rotateKey.SetActive(true);
-            if (doorObject != null)
-            {
-                Vector3 doorPosition = doorObject.transform.position;
-                doorPosition.y += doorMoveDistance;
-                doorObject.transform.position = doorPosition;
-            }
+
             if (Mathf.Approximately(currentAngle, targetAngle))
             {
-                
+                Quaternion targetRotation = Quaternion.Euler(rotateKey.transform.rotation.eulerAngles.x, rotateKey.transform.rotation.eulerAngles.y, targetAngle);
+                rotateKey.transform.rotation = targetRotation;
+
+                Invoke("MoveDoor", 1f);
             }
+        }
+    }
+    void MoveDoor()
+    {
+        if (doorObject != null)
+        {
+            Vector3 doorPosition = doorObject.transform.position;
+            doorPosition.y += doorMoveDistance;
+            doorObject.transform.position = doorPosition;
         }
     }
 }
