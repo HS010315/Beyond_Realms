8bc0b4b21cbb2bba278587f850250c8fe44b7aae
HS010315 <hspower01@gmail.com>
2024-03-13 11:07:49 +0900

    Feat : "키 꽂았을때 회전으로 전환"
    진행 중

--- Beyond Realms/Assets/03.Scripts/KeyInteraction.cs
+++ Beyond Realms/Assets/03.Scripts/KeyInteraction.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public class KeyInteraction : MonoBehaviour
 {
-    // 문 오브젝트 변수
     public GameObject doorObject;
-
-    // 문의 Y 축 이동 거리
     public float doorMoveDistance = -1.6f;
+    public GameObject rotateKey;
+    public float targetAngle = 0f;
 
     private void OnCollisionEnter(Collision collision)
     {
         GameObject gameObject = collision.gameObject;
         if (gameObject.CompareTag("Key"))
         {
+            float currentAngle = rotateKey.transform.rotation.eulerAngles.z;
             Destroy(gameObject);
-            // 문이 존재하는지 확인 후 이동
+            rotateKey.SetActive(true);
             if (doorObject != null)
             {
-                // 문의 현재 위치
                 Vector3 doorPosition = doorObject.transform.position;
-                // 새로운 Y 포지션 설정
                 doorPosition.y += doorMoveDistance;
-                // 문의 위치 업데이트
                 doorObject.transform.position = doorPosition;
             }
-            
+            if (Mathf.Approximately(currentAngle, targetAngle))
+            {
+                
+            }
         }
-
     }
 }
