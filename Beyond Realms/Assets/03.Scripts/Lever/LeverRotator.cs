60bd5a46aca4977a84483a41db50560ad796e5c6
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-05-14 13:42:05 +0900

    Scene 1 퍼즐1 애니메이터 구현

---
+++ Beyond Realms/Assets/03.Scripts/Lever/LeverRotator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverRotator : MonoBehaviour
+{
+    public GameObject leverObject;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 레버의 회전 각도를 얻어옴
+        float leverRotationX = leverObject.transform.rotation.eulerAngles.x;
+
+        // 레버의 x축 각도가 -45도 이하이면 애니메이터 변수를 1로 설정
+        if (leverRotationX <= 315f && leverRotationX >= 315f - 90f) 
+        {
+            animator.SetInteger("TableAni", 1);
+        }
+        else
+        {
+            // -45도가 아니면 애니메이터 변수를 0으로 설정
+            animator.SetInteger("TableAni", 0);
+        }
+    }
+}
