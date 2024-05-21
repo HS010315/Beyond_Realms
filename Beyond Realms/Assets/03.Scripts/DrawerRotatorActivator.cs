dff12fd3ce68ca06a989a16ddef5fffea9e0c1fe
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-05-22 01:07:02 +0900

    Fix: 레버 스크립트 수정

--- Beyond Realms/Assets/03.Scripts/DrawerRotatorActivator.cs
+++ Beyond Realms/Assets/03.Scripts/DrawerRotatorActivator.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class DrawerRotatorActivator : MonoBehaviour
 {
-    public Transform object1; 
-    public Transform object2; 
-    public DrawerRotator drawerRotator; 
+    public Transform object1;
+    public Transform object2;
+    public DrawerRotator drawerRotator;
 
     void Update()
     {
-        if (Mathf.Abs(object1.eulerAngles.z - 180f) < 0.001f && Mathf.Abs(object2.eulerAngles.z - 180f) < 0.001f)
-        {            
+        if (Mathf.Abs(object1.eulerAngles.z - 180f) < 10f && Mathf.Abs(object2.eulerAngles.z - 180f) < 10f)
+        {
             if (drawerRotator != null && !drawerRotator.enabled)
             {
                 drawerRotator.enabled = true;
