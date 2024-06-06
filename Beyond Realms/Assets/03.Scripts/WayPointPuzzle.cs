cc2277b806616606137b1464f90b28f30df6c80f
HS010315 <hspower01@gmail.com>
2024-06-07 00:48:58 +0900

    [Feat] 콜라이더 추가/waypointpuzzle 스크립트 수정

--- Beyond Realms/Assets/03.Scripts/WayPointPuzzle.cs
+++ Beyond Realms/Assets/03.Scripts/WayPointPuzzle.cs
@@ -8,7 +8,7 @@ public class WayPointPuzzle : MonoBehaviour
     public GameObject joyStickZ;                //Z축 조작하는 조이스틱
     public List<GameObject> waypoints;
     public float speed = 1f;
-    public int nowWayPointsIndex = 1;
+    public int nowWayPointsIndex = 0;
     /*public Vector3 joystic_rot_normal; //회전 값을 볼 변수
     bool moving = false;   
     List<Vector3> points_pos = new List<Vector3>();
@@ -93,29 +93,29 @@ public class WayPointPuzzle : MonoBehaviour
         //Vector3 wayPointNum = waypoints[nowWayPointsIndex].transform.position;
         switch(nowWayPointsIndex)
         {
+            case 0:
+                nowWayPointsIndex = 1;
+                break;
             case 1:
                 nowWayPointsIndex = 2;
                 break;
-            case 2:
-                nowWayPointsIndex = 3;
+            case 3:
+                nowWayPointsIndex = 5;
                 break;
             case 4:
-                nowWayPointsIndex = 6;
+                nowWayPointsIndex = 3;
                 break;
             case 5:
-                nowWayPointsIndex = 4;
+                nowWayPointsIndex = 6;
                 break;
-            case 6:
-                nowWayPointsIndex = 7;
+            case 7:
+                nowWayPointsIndex = 10;
                 break;
-            case 8:
-                nowWayPointsIndex = 11;
+            case 11:
+                nowWayPointsIndex = 12;
                 break;
             case 12:
-                nowWayPointsIndex = 13;
-                break;
-            case 13:
-                nowWayPointsIndex = 15;
+                nowWayPointsIndex = 14;
                 break;
         }
         //Vector3.MoveTowards(movingObject.transform.position, wayPointNum, speed * Time.deltaTime);
@@ -125,29 +125,29 @@ public class WayPointPuzzle : MonoBehaviour
         Vector3 wayPointNum = waypoints[nowWayPointsIndex].transform.position;
         switch (nowWayPointsIndex)
         {
+            case 1:
+                nowWayPointsIndex = 0;
+                break;
             case 2:
                 nowWayPointsIndex = 1;
                 break;
             case 3:
-                nowWayPointsIndex = 2;
-                break;
-            case 4:
-                nowWayPointsIndex = 5;
-                break;
-            case 6:
                 nowWayPointsIndex = 4;
                 break;
-            case 7:
-                nowWayPointsIndex = 6;
+            case 5:
+                nowWayPointsIndex = 3;
                 break;
-            case 11:
-                nowWayPointsIndex = 8;
+            case 6:
+                nowWayPointsIndex = 5;
                 break;
-            case 13:
+            case 10:
+                nowWayPointsIndex = 7;
+                break;
+            case 12:
+                nowWayPointsIndex = 11;
+                break;
+            case 14:
                 nowWayPointsIndex = 12;
-                break;
-            case 15:
-                nowWayPointsIndex = 13;
                 break;
         }
         Vector3.MoveTowards(movingObject.transform.position, wayPointNum, speed * Time.deltaTime);
@@ -157,26 +157,26 @@ public class WayPointPuzzle : MonoBehaviour
         Vector3 wayPointNum = waypoints[nowWayPointsIndex].transform.position;
         switch(nowWayPointsIndex)
         {
-            case 4:
-                nowWayPointsIndex = 2;
-                break;
-            case 9:
-                nowWayPointsIndex = 6;
-                break;
-            case 7:
-                nowWayPointsIndex = 10;
+            case 3:
+                nowWayPointsIndex = 1;
                 break;
             case 8:
+                nowWayPointsIndex = 5;
+                break;
+            case 6:
+                nowWayPointsIndex = 9;
+                break;
+            case 7:
+                nowWayPointsIndex = 3;
+                break;
+            case 11:
                 nowWayPointsIndex = 4;
                 break;
             case 12:
-                nowWayPointsIndex = 5;
+                nowWayPointsIndex = 13;
                 break;
-            case 13:
-                nowWayPointsIndex = 14;
-                break;
-            case 15:
-                nowWayPointsIndex = 16;
+            case 14:
+                nowWayPointsIndex = 15;
                 break;
         }
         Vector3.MoveTowards(movingObject.transform.position, wayPointNum, speed * Time.deltaTime);
@@ -186,26 +186,26 @@ public class WayPointPuzzle : MonoBehaviour
         Vector3 wayPointNum = waypoints[nowWayPointsIndex].transform.position;
         switch(nowWayPointsIndex)
         {
-            case 2:
-                nowWayPointsIndex = 4;
-                break;
-            case 6:
-                nowWayPointsIndex = 9;
-                break;
-            case 10:
-                nowWayPointsIndex = 7;
+            case 1:
+                nowWayPointsIndex = 3;
                 break;
             case 5:
-                nowWayPointsIndex = 12;
-                break;
-            case 4:
                 nowWayPointsIndex = 8;
                 break;
-            case 14:
-                nowWayPointsIndex = 13;
+            case 9:
+                nowWayPointsIndex = 6;
                 break;
-            case 16:
-                nowWayPointsIndex = 15;
+            case 4:
+                nowWayPointsIndex = 11;
+                break;
+            case 3:
+                nowWayPointsIndex = 7;
+                break;
+            case 13:
+                nowWayPointsIndex = 12;
+                break;
+            case 15:
+                nowWayPointsIndex = 14;
                 break;
         }
         Vector3.MoveTowards(movingObject.transform.position, wayPointNum, speed * Time.deltaTime);
