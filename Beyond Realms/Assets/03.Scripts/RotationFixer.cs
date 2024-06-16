e5d9431bcca6892d0123f236204db61ec38633fd
HS010315 <hspower01@gmail.com>
2024-06-17 02:09:24 +0900

    [Feat] 퍼즐에 따른 씬 스크립트

--- Beyond Realms/Assets/03.Scripts/RotationFixer.cs
+++ Beyond Realms/Assets/03.Scripts/RotationFixer.cs
@@ -4,11 +4,20 @@ public class RotationFixer : MonoBehaviour
 {
     // 원하는 양수 각도들의 배열
     public float[] targetAngles = { 60f, 120f, 180f, 240f, 300f, 360f };
-
     // 회전축을 고정할 각도 범위
     public float angleTolerance = 5f;
 
-    // 회전축을 고정할 함수
+    public Transform startPoint;
+    public Transform endPoint;
+
+    void Update()
+    {
+        // 현재 로컬 y 축 각도 얻기
+        float currentAngle = transform.localEulerAngles.y;
+
+        // 각도를 회전축 고정 함수에 전달
+        FixRotationAxis(currentAngle);
+    }
     void FixRotationAxis(float currentAngle)
     {
         // 현재 각도를 양수로 변환
@@ -34,14 +43,27 @@ public class RotationFixer : MonoBehaviour
 
         // 가장 가까운 목표 각도로 회전축을 고정
         transform.localEulerAngles = new Vector3(0f, closestTargetAngle, 0f);
+        RotatePath(closestTargetAngle);
+    } 
+
+    private void RotatePath(float angle)
+    {
+        // 경로의 중심을 기준으로 회전
+        Vector3 pathCenter = (startPoint.position + endPoint.position) / 2.0f;
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+
+        startPoint.position = RotatePointAroundPivot(startPoint.position, pathCenter, rotation);
+        endPoint.position = RotatePointAroundPivot(endPoint.position, pathCenter, rotation);
     }
 
-    void Update()
+    private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
     {
-        // 현재 로컬 y 축 각도 얻기
-        float currentAngle = transform.localEulerAngles.y;
-
-        // 각도를 회전축 고정 함수에 전달
-        FixRotationAxis(currentAngle);
+        // 점을 피벗 위치로 이동
+        Vector3 dir = point - pivot;
+        // 방향을 회전
+        dir = rotation * dir;
+        // 회전된 점을 다시 원래 위치로 이동
+        point = dir + pivot;
+        return point;
     }
 }
