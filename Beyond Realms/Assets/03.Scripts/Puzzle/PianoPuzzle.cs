fd8e3c3a9d3478a493278bb1fca5b81fc8d57ae5
HS010315 <hspower01@gmail.com>
2024-06-19 01:23:35 +0900

    [Feat] 피아노 퍼즐 완료

---
+++ Beyond Realms/Assets/03.Scripts/Puzzle/PianoPuzzle.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PianoPuzzle : MonoBehaviour
+{
+    [SerializeField] private float threshold = 0.05f;
+    [SerializeField] private float deadZone = 0.005f;
+
+    public static PianoPuzzle instance;
+
+    public int passwordNumber = 1;
+    public UnityEvent PianoPressed;
+
+    private Vector3 startPos;
+    private ConfigurableJoint joint;
+
+    //AudioSource sound;
+
+    public bool isPressed = false;
+
+    private void Awake()
+    {
+        if (PianoPuzzle.instance == null)
+        {
+            PianoPuzzle.instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sound = GetComponent<AudioSuorce>();
+        startPos = transform.localPosition;
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPressed && GetValue() + threshold >= 1)
+        {
+            Pressed();
+        }
+        if (!isPressed && GetValue() + threshold >= 1)
+        {
+            Pressed();
+        }
+        if (isPressed && GetValue() - threshold <= 0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(startPos, transform.localPosition) / joint.linearLimit.limit;
+
+        if (Mathf.Abs(value) < deadZone)
+            value = 0;
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+    private void Pressed()
+    {
+        isPressed = true;
+        //sound.Play();
+        PianoPressed.Invoke();
+    }
+    private void Released()
+    {
+        isPressed = false;
+    }
+
+    public void CorrectSound()
+    {
+    }
+    public void InCorrectSound()
+    {
+    }
+}
---
+++ Beyond Realms/Assets/03.Scripts/Puzzle/PianoPuzzleSolver.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoPuzzleSolver : MonoBehaviour
+{
+    public byte[] tilePassword = { 7, 10, 3, 5, 4, 6 };
+    private List<byte> userTileClicked = new List<byte>();
+    public PianoPuzzle[] piano;
+
+    public GameObject[] pokeColliders;
+
+    private void Start()
+    {
+        userTileClicked.Clear();
+    }
+
+    public void AddNumber(string number)
+    {
+        byte parsedNumber;
+        if (!byte.TryParse(number, out parsedNumber))
+        {
+            return;
+        }
+
+        // 리스트에 추가
+        userTileClicked.Add(parsedNumber);
+        Debug.Log("Current userTileClicked: " + string.Join(", ", userTileClicked));
+
+        // 비밀번호 길이와 비교
+        if (userTileClicked.Count == tilePassword.Length)
+        {
+            CheckPassword();
+        }
+        else if (userTileClicked.Count > tilePassword.Length)
+        {
+            // 필요한 경우, 초과된 숫자를 제거하거나 리스트를 초기화할 수 있습니다.
+            userTileClicked.Clear();
+        }
+    }
+
+    private void CheckPassword()
+    {
+        // 비밀번호가 일치하는지 확인
+        bool isCorrect = true;
+        for (byte i = 0; i < tilePassword.Length; i++)
+        {
+            if (userTileClicked[i] != tilePassword[i])
+            {
+                isCorrect = false;
+                break;
+            }
+        }
+
+        if (isCorrect)
+        {
+            // 비밀번호가 맞는 경우의 처리
+            foreach (PianoPuzzle tile in piano)
+            {
+                tile.CorrectSound();
+            }
+            Debug.Log("딩동댕");
+            pokeColliders[0].SetActive(false);
+            pokeColliders[1].SetActive(false);
+        }
+        else
+        {
+            // 비밀번호가 틀린 경우의 처리
+            foreach (PianoPuzzle tile in piano)
+            {
+                tile.InCorrectSound();
+            }
+            Debug.Log("땡");
+            userTileClicked.Clear();
+        }
+    }
+}
