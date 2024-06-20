c1cf54d1206ed38b3e6fde9aabe1159d62278aa1
HS010315 <hspower01@gmail.com>
2024-06-21 05:15:22 +0900

    [feat]중간 저장

--- Beyond Realms/Assets/03.Scripts/Laser/LaserPrefabver.cs
+++ Beyond Realms/Assets/03.Scripts/Laser/LaserPrefabver.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class LaserPrefabver : MonoBehaviour
 {
-    public Transform shootPoint;
-    public GameObject laserPrefab;
+    public GameObject laserPrefab;         // 발사할 레이저 프리팹
+    public Transform laserSpawnPoint;      // 레이저 발사 위치
+    public float laserSpeed = 20f;          // 레이저 발사 속도
+    public float maxLaserLength = 105f;     // 레이저의 최대 길이
+
+    private GameObject currentLaser;
+    private Vector3 originalScale;
+    private Vector3 targetScale;
+    private bool isShooting = false;
+
     void Start()
     {
-        
+        originalScale = laserPrefab.transform.localScale;
+        targetScale = new Vector3(originalScale.x, originalScale.y, maxLaserLength);
+        StartCoroutine(FireLaser());
     }
 
-    void Update()
+    IEnumerator FireLaser()
     {
-        
+        isShooting = true;
+        currentLaser = Instantiate(laserPrefab, laserSpawnPoint.position, laserSpawnPoint.rotation);
+        currentLaser.transform.localScale = new Vector3(originalScale.x, originalScale.y, 0);
+
+        float currentLength = 0f;
+        while (currentLength < maxLaserLength)
+        {
+            currentLength += laserSpeed * Time.deltaTime;
+            float clampedLength = Mathf.Clamp(currentLength, 0f, maxLaserLength);
+            currentLaser.transform.localScale = new Vector3(originalScale.x, originalScale.y, clampedLength);
+            yield return null;
+        }
+
+        Destroy(currentLaser);
+        isShooting = false;
     }
 }
